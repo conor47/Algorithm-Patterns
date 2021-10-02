f8e459c5b94d5b417a60db8a74619d30825d60d5
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-10-02 01:49:46 +0100

    fast and slow pointers problems

--- Pattern-FastAndSlowPointers/startOfLinkedListCycle.js
+++ Pattern-FastAndSlowPointers/startOfLinkedListCycle.js
@@ -8,8 +8,51 @@ class Node {
 }
 
 const find_cycle_start = function (head) {
-  return head;
+  let fast = head;
+  let slow = head;
+  lengthOfCycle;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      // calculte the length of the cycle
+      cycleLength = calculateCycleLength(slow);
+    }
+  }
+  slow = head;
+  fast = head;
+
+  // move one pointer ahead of the other by the number of nodes which is the length of the cycle
+  while (lengthOfCycle > 0) {
+    fast = fast.next;
+    lengthOfCycle--;
+  }
+  // loop until the pointers meet. When they meet we know we are at the head of the cycle
+  while (fast !== slow) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  return fast;
 };
+
+function calculateCycleLength(slow) {
+  let current = slow;
+  let count = 0;
+
+  while (true) {
+    current = current.next;
+    count += 1;
+    if (current === slow) {
+    }
+  }
+  return count;
+}
+
+// Time complexity is O(N). Finding the length of the cycle is O(N) and finding the head of the cycle is O(N)
+
+// Space complexity is O(1). Constant space
 
 head = new Node(1);
 head.next = new Node(2);
