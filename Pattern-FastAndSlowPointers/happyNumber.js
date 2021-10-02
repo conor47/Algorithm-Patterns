c06cf09a51e9fa87d01c3f0400e5cd2caf7c2b03
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-10-02 16:53:20 +0100

    fast and slow pointer problems

---
+++ Pattern-FastAndSlowPointers/happyNumber.js
@@ -0,0 +1,74 @@
+// Any number will be called a happy number if, after repeatedly replacing it with a number equal to the sum of the square of all of its digits, leads us to number ‘1’. All other (not-happy) numbers will never reach ‘1’. Instead, they will be stuck in a cycle of numbers which does not include ‘1’.
+
+// Example 1:
+
+// Input: 23
+// Output: true (23 is a happy number)
+// Explanations: Here are the steps to find out that 23 is a happy number:
+
+//     22+322^2 + 3 ^22​2​​+3​2​​ = 4 + 9 = 13
+//     12+321^2 + 3^21​2​​+3​2​​ = 1 + 9 = 10
+//     12+021^2 + 0^21​2​​+0​2​​ = 1 + 0 = 1
+
+// Example 2:
+
+// Input: 12
+// Output: false (12 is not a happy number)
+// Explanations: Here are the steps to find out that 12 is not a happy number:
+
+//     12+221^2 + 2 ^21​2​​+2​2​​ = 1 + 4 = 5
+//     525^25​2​​ = 25
+//     22+522^2 + 5^22​2​​+5​2​​ = 4 + 25 = 29
+//     22+922^2 + 9^22​2​​+9​2​​ = 4 + 81 = 85
+//     82+528^2 + 5^28​2​​+5​2​​ = 64 + 25 = 89
+//     82+928^2 + 9^28​2​​+9​2​​ = 64 + 81 = 145
+//     12+42+521^2 + 4^2 + 5^21​2​​+4​2​​+5​2​​ = 1 + 16 + 25 = 42
+//     42+224^2 + 2^24​2​​+2​2​​ = 16 + 4 = 20
+//     22+022^2 + 0^22​2​​+0​2​​ = 4 + 0 = 4
+//     424^24​2​​ = 16
+//     12+621^2 + 6^21​2​​+6​2​​ = 1 + 36 = 37
+//     32+723^2 + 7^23​2​​+7​2​​ = 9 + 49 = 58
+//     52+825^2 + 8^25​2​​+8​2​​ = 25 + 64 = 89
+
+// Step ‘13’ leads us back to step ‘5’ as the number becomes equal to ‘89’, this means that we can never reach ‘1’, therefore, ‘12’ is not a happy number.
+
+function happyNumber(num) {
+  let slow = num;
+  let fast = num;
+  while (true) {
+    slow = findSquareSum(num);
+    fast = findSquareSum(findSquareSum(num));
+    if (fast === slow) {
+      break;
+    }
+  }
+  return slow === 1;
+}
+
+// function for calculating the square sum of a given number.
+function findSquareSum(num) {
+  let sum = 0;
+  while (num > 0) {
+    digit = num % 10;
+    sum += digit * digit;
+    num = Math.floor(num / 10);
+  }
+  return sum;
+}
+
+// The time complexity of the algorithm is difficult to determine. However we know the fact that all unhappy numbers eventually get stuck in the cycle: 4 -> 16 -> 37 -> 58 -> 89 -> 145 -> 42 -> 20 -> 4
+
+// This sequence behavior tells us two things:
+
+//     If the number N is less than or equal to 1000, then we reach the cycle or ‘1’ in at most 1001 steps.
+//     For N>1000, suppose the number has ‘M’ digits and the next number is ‘N1’. From the above Wikipedia link, we know that the sum of the squares of the digits of ‘N’ is at most 92M9^2M9​2​​M, or 81M81M81M (this will happen when all digits of ‘N’ are ‘9’).
+
+// This means:
+
+//     N1<81MN1 < 81MN1<81M
+//     As we know M=log(N+1)M = log(N+1)M=log(N+1)
+//     Therefore: N1<81∗log(N+1)N1 < 81 * log(N+1)N1<81∗log(N+1) => N1=O(logN)N1 = O(logN)N1=O(logN)
+
+// This concludes that the above algorithm will have a time complexity of O(logN)O(logN)O(logN).
+
+// Space complexity is O(1)
