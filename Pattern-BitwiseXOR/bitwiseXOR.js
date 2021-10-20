0acf27cf44cb6241d4a72b68e58e903044db1d87
Conor ladrigan <conorladrigan95@gmail.com>
2021-10-20 17:30:49 +0100

    Bitwise XOR problems

--- Pattern-BitwiseXOR/bitwiseXOR.js
+++ Pattern-BitwiseXOR/bitwiseXOR.js
@@ -8,11 +8,13 @@
 function find_missing_number(arr) {
   let n = arr.length + 1;
 
+  //   x1 is the XOR of all values from 1-n
   let x1 = 1;
   for (let i = 2; i <= n; i++) {
     x1 = x1 ^ i;
   }
 
+  //   x2 is the XOR of all the values in the array
   let x2 = arr[0];
   for (let i = 1; i < n - 1; i++) {
     x2 = x2 ^ arr[i];
