1c49198fa2f8ed17b642ea780f5adbbde5f763bd
Conor ladrigan <conorladrigan95@gmail.com>
2021-10-22 16:29:39 +0100

    k way merge problems

--- Pattern-KWayMerge/kthSmallestNumberInASortedMatrix.js
+++ Pattern-KWayMerge/kthSmallestNumberInASortedMatrix.js
@@ -11,11 +11,34 @@
 // Output: 7
 // Explanation: The 5th smallest number in the matrix is 7.
 
-const find_Kth_smallest = function (matrix, k) {
-  number = -1;
+const heap = require('collections/heap');
 
+const find_Kth_smallest = function (matrix, k) {
+  let minHeap = new heap([], null, (a, b) => b[0] - a[0]);
+
+  for (let i = 0; i < matrix.length; i++) {
+    minHeap.push([matrix[i][0], 0, matrix[i]]);
+  }
+
+  let numberCount = 0;
+  let number = 0;
+
+  while (minHeap.length > 0) {
+    [number, i, list] = minHeap.pop();
+    numberCount += 1;
+    if (numberCount === k) {
+      break;
+    }
+    if (list[i + 1]) {
+      minHeap.push([list[i + 1], i + 1, list]);
+    }
+  }
   return number;
 };
+
+// Time complexity is O()
+
+// Space complexity is O(N) as in the worst case we store one number from each row in the heap
 
 console.log(
   `Kth smallest number is: ${find_Kth_smallest(
