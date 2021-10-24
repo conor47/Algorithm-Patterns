1c637ed2533cba513f438bdbe340dbaca4316930
Conor ladrigan <conorladrigan95@gmail.com>
2021-10-24 15:33:29 +0100

    recursion problems

---
+++ Strategy-Recursion/decimalToBinary.js
@@ -0,0 +1,14 @@
+// Write a function that takes a number testVariable and returns a string that is its equivalent binary number.
+
+function decimalToBinary(testVariable) {
+  if (testVariable <= 1) {
+    return String(testVariable);
+  } else {
+    return (
+      decimalToBinary(Math.floor(testVariable / 2)) +
+      decimalToBinary(testVariable % 2)
+    );
+  }
+}
+
+console.log(decimalToBinary(10));
