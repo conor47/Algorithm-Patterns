1b0e40486a31fdef05e33294947569c860a57009
Conor ladrigan <conorladrigan95@gmail.com>
2021-10-25 00:48:46 +0100

    recursion problems

---
+++ Strategy-Recursion/sortAnArray.js
@@ -0,0 +1,16 @@
+function sort(testVariable, length) {
+  if (length <= 1) {
+    return;
+  }
+
+  sort(testVariable, length - 1);
+
+  let lastElement = testVariable[length - 1];
+  let temp = length - 2;
+
+  while (temp >= 0 && testVariable[temp] > lastElement) {
+    testVariable[temp + 1] = testVariable[temp];
+    temp = temp - 1;
+  }
+  testVariable[temp + 1] = lastElement;
+}
