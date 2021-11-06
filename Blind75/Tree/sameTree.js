6b6273c0ce12f6126f0f3ebe6112dc414f14a9c4
Conor ladrigan <conorladrigan95@gmail.com>
2021-11-06 19:17:01 +0000

    blind75 problems

---
+++ Blind75/Tree/sameTree.js
@@ -0,0 +1,14 @@
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+let isSameTree = function (p, q) {
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+  if (p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+// space complexity is O(N) sine each node is visited in
+
+// space complextiy is O(N) in the worst case and O(log N) in the best case
