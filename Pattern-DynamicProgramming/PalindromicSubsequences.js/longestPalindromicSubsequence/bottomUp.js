23d97be59835c20dad532743063f7c7a57359cc8
Conor ladrigan <conorladrigan95@gmail.com>
2021-11-13 02:27:48 +0000

    dynamic programming problems

--- Pattern-DynamicProgramming/PalindromicSubsequences.js/longestPalindromicSubsequence/bottomUp.js
+++ Pattern-DynamicProgramming/PalindromicSubsequences.js/longestPalindromicSubsequence/bottomUp.js
@@ -10,7 +10,23 @@ const findLPSLength = function (st) {
   for (let startIndex = st.length - 1; startIndex >= 0; startIndex--) {
     for (let endIndex = startIndex + 1; endIndex < st.length; endIndex++) {
       if (st[endIndex] === st[startIndex]) {
+        dp[startIndex][endIndex] = 2 + dp[startIndex + 1][endIndex - 1];
+      } else {
+        dp[startIndex][endIndex] = Math.max(
+          dp[startIndex][endIndex - 1],
+          dp[startIndex + 1][endIndex - 1]
+        );
       }
     }
   }
+
+  return dp[0][dp.length - 1];
 };
+
+// Time complexity is O(n^2)
+
+// Space complexity is O(n^2)
+
+console.log('Length of LPS ---> ' + findLPSLength('abdbca'));
+console.log('Length of LPS ---> ' + findLPSLength('cddpd'));
+console.log('Length of LPS ---> ' + findLPSLength('pqr'));
