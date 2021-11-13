a36de952bcc73bb8f00ec29ab30120bedf995c70
Conor ladrigan <conorladrigan95@gmail.com>
2021-11-13 00:06:33 +0000

    blind 75 problems

---
+++ Blind75/Matrix/setMatrixZeros.js
@@ -0,0 +1,43 @@
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+
+// You must do it in place.
+
+// Input: matrix = [
+//   [0, 1, 2, 0],
+//   [3, 4, 5, 2],
+//   [1, 3, 1, 5],
+// ];
+// Output: [
+//   [0, 0, 0, 0],
+//   [0, 4, 5, 0],
+//   [0, 3, 1, 0],
+// ];
+
+// Constraints: m == matrix.length;
+// n == matrix[0].length;
+// 1 <= m, n <= 200 - 231 <= matrix[i][j] <= 231 - 1;
+
+const setZeroes = function (matrix) {
+  let row = new Set();
+  let col = new Set();
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        row.add(i);
+        col.add(j);
+      }
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (row.has(i) || col.has(j)) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+};
+
+// Time complexity is O(M * N) where M and N are the dimensions of the matrik
+
+// Space complexity is O(M + N) since we are keeping a set of each column and row
---
+++ LinkedList/test.py
@@ -0,0 +1,4 @@
+test = [[1,2], [3,4], [5,6]]
+
+for idx,list in enumerate(test):
+    print(idx,list)
