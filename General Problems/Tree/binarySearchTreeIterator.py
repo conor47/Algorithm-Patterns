137bd045e2189ccef64fe2533240f0d78e066700
Conor ladrigan <conorladrigan95@gmail.com>
2021-12-14 17:41:22 +0000

    tree problems

---
+++ General Problems/Tree/binarySearchTreeIterator.py
@@ -0,0 +1,34 @@
+# Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+#     BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+#     boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+#     int next() Moves the pointer to the right, then returns the number at the pointer.
+
+# Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+# You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.sorted = []
+        self.pos = -1
+        self.inorder(root)
+        
+    def inorder(self,node):
+        if not node:
+            return
+        self.inorder(node.left)
+        self.sorted.append(node.val)
+        self.inorder(node.right)
+
+    def next(self) -> int:
+        self.pos += 1
+        return self.sorted[self.pos]
+
+    def hasNext(self) -> bool:
+        return not self.pos == len(self.sorted) - 1
+
+# Time complexity is O(N) where N is the number of nodes in the tree
+
+# Space complexity is O(N)
