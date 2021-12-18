09581ac69d3f88da02565e7897b0ad0d888a416a
Conor ladrigan <conorladrigan95@gmail.com>
2021-12-18 16:49:23 +0000

    stack problems

---
+++ General Problems/Stack/carFleet.py
@@ -0,0 +1,54 @@
+# Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+# Output: 3
+# Explanation:
+# The cars starting at 10 and 8 become a fleet, meeting each other at 12.
+# The car starting at 0 doesn't catch up to any other car, so it is a fleet by itself.
+# The cars starting at 5 (speed 3) and 3 (speed 1) become a fleet, meeting each other at 6. The fleet moves at speed 1 until it reaches target.
+# Note that no other cars meet these fleets before the destination, so the answer is 3.
+
+# Example 2:
+
+# Input: target = 10, position = [3], speed = [3]
+# Output: 1
+# Explanation: There is only one car, hence there is only one fleet.
+
+# Example 3:
+
+# Input: target = 100, position = [0,2,4], speed = [4,2,1]
+# Output: 1
+# Explanation:
+# The cars starting at 0 (speed 4) and 2 (speed 2) become a fleet, meeting each other at 4. The fleet moves at speed 2.
+# Then, the fleet (speed 2) and the car starting at 4 (speed 1) become one fleet, meeting each other at 6. The fleet moves at speed 1 until it reaches target.
+
+ 
+
+# Constraints:
+
+#     n == position.length == speed.length
+#     1 <= n <= 105
+#     0 < target <= 106
+#     0 <= position[i] < target
+#     All the values of position are unique.
+#     0 < speed[i] <= 106
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+        
+        combined = [(pos,s) for pos,s in zip(position,speed)]
+        
+        for pos,speed in sorted(combined)[::-1]:
+           
+            if not stack:
+                stack.append((target-pos) / speed)
+                continue
+
+            time = (target - pos) / speed
+
+            if time > stack[-1]:
+                stack.append(time)
+        return len(stack)
+
+# Time complexity is O(N)
+
+# Space complexity is O(N)
