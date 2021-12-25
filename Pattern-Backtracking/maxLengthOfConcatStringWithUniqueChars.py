c85d479e929a582556d0b8a9bbeb7e79b8ed6e1d
Conor ladrigan <conorladrigan95@gmail.com>
2021-12-25 00:33:04 +0000

    backtracking problems

---
+++ Pattern-Backtracking/maxLengthOfConcatStringWithUniqueChars.py
@@ -0,0 +1,68 @@
+# You are given an array of strings arr. A string s is formed by the concatenation of a subsequence of arr that has unique characters.
+
+# Return the maximum possible length of s.
+
+# A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+# Example 1:
+
+# Input: arr = ["un","iq","ue"]
+# Output: 4
+# Explanation: All the valid concatenations are:
+# - ""
+# - "un"
+# - "iq"
+# - "ue"
+# - "uniq" ("un" + "iq")
+# - "ique" ("iq" + "ue")
+# Maximum length is 4.
+
+# Example 2:
+
+# Input: arr = ["cha","r","act","ers"]
+# Output: 6
+# Explanation: Possible longest valid concatenations are "chaers" ("cha" + "ers") and "acters" ("act" + "ers").
+
+# Example 3:
+
+# Input: arr = ["abcdefghijklmnopqrstuvwxyz"]
+# Output: 26
+# Explanation: The only string in arr has all 26 characters.
+
+ 
+
+# Constraints:
+
+#     1 <= arr.length <= 16
+#     1 <= arr[i].length <= 26
+#     arr[i] contains only lowercase English letters.
+
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        ans = 0
+        res_map = Counter()
+        
+        def backtrack(res_map, start, end):
+            if len(res_map) and res_map.most_common(1)[0][1] > 1:
+                return
+            nonlocal ans
+            ans = max(ans,len(res_map)) 
+            for i in range(start,end):
+                    word_map = Counter(arr[i])
+                    if len(word_map) != len(arr[i]):
+                        continue
+                    res_map.update(word_map)
+                    backtrack(res_map,i+1,end)
+                    for c in word_map:
+                        if res_map[c] == word_map[c]:
+                            del res_map[c]
+                        else:
+                            res_map[c] -= word_map[c]
+        backtrack(res_map,0,len(arr))       
+        return ans
+
+# Time complexity is O(2^N)
+
+# Space complexity is O(N) for the recursion stack and the counters
