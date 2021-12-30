fefa49d3e2cf2c32d9443a0a641a18da183ea167
Conor ladrigan <conorladrigan95@gmail.com>
2021-12-30 08:41:15 +0000

    dynamic programming problems

--- General Problems/DynamicProgramming/minimumFallingPathSum.py
+++ General Problems/DynamicProgramming/minimumFallingPathSum.py
@@ -20,3 +20,25 @@ class Solution:
         for i in range(n):
             m = min(m,recursive(0,i))
         return m
+
+# Bottom up solution
+
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        n = len(matrix)
+        if n ==1:
+            return min(matrix[0])
+        dp = [[0 for i in range(n)] for _ in range(n)]
+        
+        for i in range(n):
+            dp[0][i] = matrix[0][i]
+        
+        for i in range(1,n):
+            for j in range(n):
+                if j  == 0:
+                    dp[i][j] = matrix[i][j]+ min(dp[i-1][j], dp[i-1][j+1])
+                elif j == n-1:
+                    dp[i][j] =matrix[i][j] + min(dp[i-1][j], dp[i-1][j-1])
+                else:
+                    dp[i][j] = matrix[i][j] + min(dp[i-1][j], dp[i-1][j+1], dp[i-1][j-1])
+        return min(dp[n-1])
