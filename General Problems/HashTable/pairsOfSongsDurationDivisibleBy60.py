8dc406e68d43f8086b21e5438efe52483b48e29c
Conor ladrigan <conorladrigan95@gmail.com>
2022-01-02 17:03:11 +0000

    hash table problems

---
+++ General Problems/HashTable/pairsOfSongsDurationDivisibleBy60.py
@@ -0,0 +1,49 @@
+# You are given a list of songs where the ith song has a duration of time[i] seconds.
+
+# Return the number of pairs of songs for which their total duration in seconds is divisible by 60. Formally, we want the number of indices i, j such that i < j with (time[i] + time[j]) % 60 == 0.
+
+ 
+
+# Example 1:
+
+# Input: time = [30,20,150,100,40]
+# Output: 3
+# Explanation: Three pairs have a total duration divisible by 60:
+# (time[0] = 30, time[2] = 150): total duration 180
+# (time[1] = 20, time[3] = 100): total duration 120
+# (time[1] = 20, time[4] = 40): total duration 60
+
+# Example 2:
+
+# Input: time = [60,60,60]
+# Output: 3
+# Explanation: All three pairs have a total duration of 120, which is divisible by 60.
+
+ 
+
+# Constraints:
+
+#     1 <= time.length <= 6 * 104
+#     1 <= time[i] <= 500
+
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        res = 0
+        counts = Counter(time)
+        for t in time:
+            inc = 60
+            while inc < 1000:
+                dif = inc - t
+                x = counts.get(dif,0)
+                if dif == t and x == 1:
+                    x = 0
+                elif dif == t:
+                    x -= 1
+                res += x
+                inc += 60
+                
+        return res //2
+
+# Time complexity is O(N)
+
+# Space complexity is O(N)
