a0e33d23f5cfbac119aefeb9289a61c36b6e4f39
Conor ladrigan <conorladrigan95@gmail.com>
2022-01-06 11:29:59 +0000

    array problems

---
+++ General Problems/Array/shuffleAnArray.py
@@ -0,0 +1,57 @@
+# Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+
+# Implement the Solution class:
+
+#     Solution(int[] nums) Initializes the object with the integer array nums.
+#     int[] reset() Resets the array to its original configuration and returns it.
+#     int[] shuffle() Returns a random shuffling of the array.
+
+ 
+
+# Example 1:
+
+# Input
+# ["Solution", "shuffle", "reset", "shuffle"]
+# [[[1, 2, 3]], [], [], []]
+# Output
+# [null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+
+# Explanation
+# Solution solution = new Solution([1, 2, 3]);
+# solution.shuffle();    // Shuffle the array [1,2,3] and return its result.
+#                        // Any permutation of [1,2,3] must be equally likely to be returned.
+#                        // Example: return [3, 1, 2]
+# solution.reset();      // Resets the array back to its original configuration [1,2,3]. Return [1, 2, 3]
+# solution.shuffle();    // Returns the random shuffling of array [1,2,3]. Example: return [1, 3, 2]
+
+ 
+
+# Constraints:
+
+#     1 <= nums.length <= 200
+#     -106 <= nums[i] <= 106
+#     All the elements of nums are unique.
+#     At most 5 * 104 calls in total will be made to reset and shuffle.
+
+class Solution:
+    def __init__(self, nums):
+        self.array = nums
+        self.original = list(nums)
+
+    def reset(self):
+        self.array = self.original
+        self.original = list(self.original)
+        return self.array
+
+    def shuffle(self):
+        aux = list(self.array)
+
+        for idx in range(len(self.array)):
+            remove_idx = random.randrange(len(aux))
+            self.array[idx] = aux.pop(remove_idx)
+
+        return self.array
+
+# Time complexity O(N^2) since the array pop calls run in O(N) time and we are calling it N times
+
+# Space complexity is O(N)
