00dc7d9327a6d76cd193eaed4107a4d40b44e8e1
Conor ladrigan <conorladrigan95@gmail.com>
2022-01-07 13:14:26 +0000

    queue problems

---
+++ General Problems/Queue/slidingWindowMax.py
@@ -0,0 +1,45 @@
+# You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+# Return the max sliding window.
+
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: 'List[int]', k: 'int') -> 'List[int]':
+        # base cases
+        n = len(nums)
+        if n * k == 0:
+            return []
+        if k == 1:
+            return nums
+        
+        def clean_deque(i):
+            # remove indexes of elements not from sliding window
+            if deq and deq[0] == i - k:
+                deq.popleft()
+                
+            # remove from deq indexes of all elements 
+            # which are smaller than current element nums[i]
+            while deq and nums[i] > nums[deq[-1]]:
+                deq.pop()
+        
+        # init deque and output
+        deq = deque()
+        max_idx = 0
+        for i in range(k):
+            clean_deque(i)
+            deq.append(i)
+            # compute max in nums[:k]
+            if nums[i] > nums[max_idx]:
+                max_idx = i
+        output = [nums[max_idx]]
+        
+        # build output
+        for i in range(k, n):
+            clean_deque(i)          
+            deq.append(i)
+            output.append(nums[deq[0]])
+        return output
+
+# Time complexity is O(N) since we are adding and removing each element at most twice 
+
+# Space complexity is O(K) for the queue and O(N-k+1) for the output list
