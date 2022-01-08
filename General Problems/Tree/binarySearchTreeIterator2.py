27c1b495d2733a63fb3a77a1699b08a54f1494b8
Conor ladrigan <conorladrigan95@gmail.com>
2022-01-08 17:21:06 +0000

    tree problems

---
+++ General Problems/Tree/binarySearchTreeIterator2.py
@@ -0,0 +1,38 @@
+# Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+#     BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+#     boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+#     int next() Moves the pointer to the right, then returns the number at the pointer.
+#     boolean hasPrev() Returns true if there exists a number in the traversal to the left of the pointer, otherwise returns false.
+#     int prev() Moves the pointer to the left, then returns the number at the pointer.
+
+# Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+# You may assume that next() and prev() calls will always be valid. That is, there will be at least a next/previous number in the in-order traversal when next()/prev() is called.
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        def inorder(r):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+        self.arr = inorder(root)
+        self.n = len(self.arr)
+        self.pointer = -1
+
+    def hasNext(self) -> bool:
+        return self.pointer < self.n - 1
+
+    def next(self) -> int:
+        self.pointer += 1
+        return self.arr[self.pointer]
+
+    def hasPrev(self) -> bool:
+        return self.pointer > 0
+
+    def prev(self) -> int:
+        self.pointer -= 1
+        return self.arr[self.pointer]
+
+# Time complexity of initialization is O(N). All other operations are O(1)
+
+# Space complexity is O(N) since we are maintaining an array of the inorder traversal of the tree
